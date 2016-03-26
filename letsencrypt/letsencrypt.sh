c25c2c6bf9b465591920fd6318fee7ef06f259c6
Rob Szumski <rob@robszumski.com>
2016-03-26 15:20:34 -0700

    letsencrypt: tweak temlate

--- letsencrypt/letsencrypt.sh
+++ letsencrypt/letsencrypt.sh
@@ -16,23 +16,19 @@ fi
 echo "DOMAINS: " $DOMAINS
 echo "EMAIL: " $EMAIL
 
-domain_args=""
-for i in "${DOMAINS[@]}"
+for domain in "${DOMAINS[@]}"
 do
-   domain_args="$domain_args -d $i"
-   # do whatever on $i
+   /usr/local/bin/letsencrypt certonly \
+    --webroot -w /letsencrypt/challenges/ \
+    --text --renew-by-default --agree-tos \
+    -d $domain \
+    --email=$EMAIL
 done
-
-# /usr/local/bin/letsencrypt certonly \
-#     --webroot -w /letsencrypt/challenges/ \
-#     --text --renew-by-default --agree-tos \
-#       $domain_args \
-#      --email=$EMAIL
 
 for domain_full in "${DOMAINS[@]}"
 do
   domain_short=`echo $domain_full | sed 's/....$//'`
-  echo "/letsencrypt/config/$domain_short.conf"
+  echo "Generating config file /letsencrypt/config/$domain_short.conf"
   cp backend.conf.template "/letsencrypt/config/$domain_short.conf"
   sed -i -e "s/%DOMAIN%/$domain_full/g" "/letsencrypt/config/$domain_short.conf"
   sed -i -e "s/%DOMAIN_SHORT%/$domain_short/g" "/letsencrypt/config/$domain_short.conf"
